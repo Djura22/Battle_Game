03d98cd5a352e0fd44456213d10cd152b3b181de
Djura22 <michaeliangroves@gmail.com>
2020-08-20 13:24:40 +0100

    P1 attack passed

--- app.rb
+++ app.rb
@@ -15,13 +15,19 @@ class Battle < Sinatra::Base
 	post '/names' do
 		session[:player_name_1] = params[:player_name_1]
 		session[:player_name_2] = params[:player_name_2]
-		redirect './play'
+		redirect '/play'
 	end
 
 	get '/play' do
 		@player_name_1 = session[:player_name_1]
 		@player_name_2 = session[:player_name_2]
 		erb :play
+	end
+
+	get '/P1_attack' do
+		@player_name_1 = session[:player_name_1]
+		@player_name_2 = session[:player_name_2]
+		erb :p1_attack
 	end
 
 	run! if app_file == $0
